871c70d33b1570e1ee7c46b493d70e6ec8db178a
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-16 16:37:42 +0530

    Update public.py

--- plugins/public.py
+++ plugins/public.py
@@ -19,6 +19,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 @Client.on_message(filters.private & filters.command(["fwd", "forward"]))
 async def run(bot, message):
     try:
+        user_id = message.from_user.id
         text = await bot.send_message(user_id, "<b><u>Set Target Chat</u></b>\n\nForward A Message From Your Target Chat\n/cancel - To Cancel This Process")
         bots_ids = await bot.listen(chat_id=user_id, timeout=300)
         if bots_ids.text=="/cancel":
@@ -30,13 +31,14 @@ async def run(bot, message):
            await bots_ids.delete()
            return await text.edit_text("This Is Not A Forward Message")
         else:
-           bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', bots_ids.text, re.IGNORECASE)
-           bot_token = bot_token[0] if bot_token else None
+           user_id = user_id
            bot_id = re.findall(r'\d[0-9]{8,10}', message.text)
            bot_id = int(bot_id[0]) if bot_id else None
+           bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', bots_ids.text, re.IGNORECASE)
+           bot_token = bot_token[0] if bot_token else None
            user_nam = re.findall(r'@[A-Za-z_-]+bot', message.text, re.IGNORECASE)
-           user_name = user_nam[0].lstrip('@') if user_nam else None
-         bots = await db.add_bot(user_id, bot_id, bot_token, username)
+           username = user_nam[0].lstrip('@') if user_nam else None
+         bots = await db.add_bot(user_id=user_id, bot_id=bot_id, bot_token=bot_token, username=username)
          await bots_ids.delete()
          await text.edit_text(
             "Successfully Updated" if bot else "This Channel Already Added",
