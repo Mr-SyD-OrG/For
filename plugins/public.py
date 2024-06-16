9c88e11437c2f0e7eeb9dddade0695535782a457
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-16 11:49:15 +0530

    Update public.py

--- plugins/public.py
+++ plugins/public.py
@@ -27,8 +27,27 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 
 @Client.on_message(filters.private & filters.command(["fwd", "forward"]))
 async def run(bot, message):
-    bot = await CLIENT.add_bot(bot, query)
-     if bot != True: return
-     await query.message.reply_text(
-        "<b>Bot Token Successfully Added To Database</b>",
-        reply_markup=InlineKeyboardMarkup(buttons))
+    try:
+         text = await bot.send_message(user_id, "<b><u>Set Target Chat</u></b>\n\nForward A Message From Your Target Chat\n/cancel - To Cancel This Process")
+         chat_ids = await bot.listen(chat_id=user_id, timeout=300)
+         if chat_ids.text=="/cancel":
+            await chat_ids.delete()
+            return await text.edit_text(
+                  "Process Canceled",
+                  reply_markup=InlineKeyboardMarkup(buttons))
+         elif not chat_ids.forward_date:
+            await chat_ids.delete()
+            return await text.edit_text("This Is Not A Forward Message")
+         else:
+            chat_id = chat_ids.forward_from_chat.id
+            title = chat_ids.forward_from_chat.title
+            username = chat_ids.forward_from_chat.username
+            username = "@" + username if username else "private"
+         chat = await db.add_bot(user_id, chat_id, title, username)
+         await chat_ids.delete()
+         await text.edit_text(
+            "Successfully Updated" if chat else "This Channel Already Added",
+            reply_markup=InlineKeyboardMarkup(buttons))
+     except asyncio.exceptions.TimeoutError:
+         await text.edit_text('Process Has Been Automatically Cancelled', reply_markup=InlineKeyboardMarkup(buttons))
+  
