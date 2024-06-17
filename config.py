d0cc301112282ef1f27b4d116843239104de43c7
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-17 15:56:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     DB_URL = os.environ.get("DB_URL", "")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
-
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 
 class temp(object): 
     lock = {}
