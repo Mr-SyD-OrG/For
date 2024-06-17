de0e31735c797f03e80ed490d51532b137c18c96
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-17 08:59:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Mr-SyD-OrG/For JishuDeveloper/Ultra-Forward-Bot
+  git clone https://github.com/Mr-SyD-OrG/For Mr-SyD-OrG/For
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Mr-SyD-OrG/For -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/Mr-SyD-OrG/For -b $BRANCH /For
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+cd Mr-SyD-OrG/For
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
