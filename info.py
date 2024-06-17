3ac1f40670b8f6d62778ba6145deb2a4641f6a4b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-17 16:19:21 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,12 @@
+import re
+from os import environ,getenv
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
